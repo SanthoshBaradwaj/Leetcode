693948a13cdfc3a3dd6a748c9d12477bd26a0956
santhoshbaradwaj <santhoshbaradwajvr306@gmail.com>
2025-04-01 12:57:39 -0700

    2nd problem

---
+++ LC605.py
@@ -0,0 +1,42 @@
+class Solution(object):
+    def canPlaceFlowers(self, flowerbed, n):
+        """
+        :type flowerbed: List[int]
+        :type n: int
+        :rtype: bool
+        """
+        length = len(flowerbed)
+        countF=0
+        for i in range(length):
+            if flowerbed[i] == 0:
+                empty_left = (i == 0) or (flowerbed[i - 1] == 0)
+                empty_right = (i == length - 1) or (flowerbed[i + 1] == 0)
+                
+                if empty_left and empty_right:
+                    flowerbed[i] = 1
+                    countF += 1
+                    if countF >= n:
+                        return True
+        return countF >= n
+
+
+# 605. Can Place Flowers
+# Solved
+# Easy
+# Topics
+# Companies
+# You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+
+# Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
+
+ 
+
+# Example 1:
+
+# Input: flowerbed = [1,0,0,0,1], n = 1
+# Output: true
+# Example 2:
+
+# Input: flowerbed = [1,0,0,0,1], n = 2
+# Output: false
+ 
